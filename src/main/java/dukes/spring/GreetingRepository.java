2be38a0ab5418e1e3c845ed013667800f22939b0
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2025-05-20 11:21:10 +0200

    Simple Spring Data example

---
+++ src/main/java/dukes/spring/GreetingRepository.java
@@ -0,0 +1,8 @@
+package dukes.spring;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GreetingRepository extends CrudRepository<Greeting, Long> {
+}
