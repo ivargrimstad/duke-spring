47309f2cbfbc14264a4ba6a5699302d92a9c11bf
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2025-05-23 11:55:19 +0200

    Fix demo to actually use CDI

--- duke-spring-jakartaee/src/main/java/dukes/spring/Application.java
+++ duke-spring-jakartaee/src/main/java/dukes/spring/Application.java
@@ -1,13 +1,29 @@
 package dukes.spring;
 
+import dukes.ai.JakartaGPT;
+import jakarta.enterprise.inject.se.SeContainer;
+import jakarta.enterprise.inject.se.SeContainerInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
+	private static SeContainer weldContainer;
+
 	public static void main(String[] args) {
+
+		weldContainer = SeContainerInitializer.newInstance()
+				.addBeanClasses(JakartaGPT.class)
+				.initialize();
+
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public JakartaGPT jakartaGPT() {
+
+		return weldContainer.select(JakartaGPT.class).get();
+	}
 }
